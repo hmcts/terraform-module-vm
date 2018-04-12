6c538dad002c69def3654ebe62cdf8b91beed701
danmerron-sig <dmerron@sportingsolutions.com>
2018-04-12 13:29:40 +0100

    fmt

--- module/variables.tf
+++ module/variables.tf
@@ -54,6 +54,7 @@ variable "delete_os_disk_on_termination" {
 variable "delete_data_disks_on_termination" {
   default = "true"
 }
+
 variable "azure_subscription_id" {}
 variable "subnet" {}
 variable "vnet" {}
