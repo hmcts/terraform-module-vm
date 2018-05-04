074d93e80882bc774e17ebd7ff5fed5e55da59c6
fdhmcts <francois.dupoux@hmcts.net>
2018-05-04 16:00:39 +0100

    RDO-1905 - Use latest CentOS-7.4 when new VMs are provisioned

--- module/main.tf
+++ module/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_virtual_machine" "reform-nonprod" {
   storage_image_reference {
     publisher = "OpenLogic"
     offer     = "CentOS"
-    sku       = "7.3"
+    sku       = "7.4"
     version   = "latest"
   }
 
@@ -64,7 +64,7 @@ resource "azurerm_virtual_machine" "reform-nonprod" {
   }
 
   lifecycle {
-    ignore_changes = ["os_profile"]
+    ignore_changes = ["os_profile", "storage_image_reference"]
   }
 
   os_profile_linux_config {
